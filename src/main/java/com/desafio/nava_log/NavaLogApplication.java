bf979604c792310ecd570d0ba3d291092523fbd0
Robson Costa <st.robson.costa@gmail.com>
2025-03-26 12:00:20 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/desafio/nava_log/NavaLogApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.nava_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavaLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavaLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/nava_log/NavaLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.nava_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavaLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
